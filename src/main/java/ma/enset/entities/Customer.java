036339a839b6236ddf9c104d2795a5a13185dcfb
Votre AIT JEDDI <assiaait2002@gmail.com>
2024-10-06 15:07:57 +0100

    second

--- src/main/java/ma/enset/entities/Customer.java
+++ src/main/java/ma/enset/entities/Customer.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Entity @AllArgsConstructor @NoArgsConstructor @Builder
 public class Customer {
     @Id
+    git add .
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
